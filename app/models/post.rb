105f13efbe161dbd2a38959f3c7cddf74e89758e
wuyepabdul <wuyepabdul@gmail.com>
2022-01-27 00:09:26 +0100

    Add update_posts_counter method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
     belongs_to :user, counter_cache: :posts_counter
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    def update_posts_counter
+        comments.last(5)
+    end
+
+    def update_posts_counter
+        user.posts_counter = 0 if user.posts_counter.nil?
+        user.posts_counter += 1
+        user.save()
+    end
 end
