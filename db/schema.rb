89a002025eb6fda7e5c74f0aa9cbbc9aa0e04335
wuyepabdul <wuyepabdul@gmail.com>
2022-01-26 23:14:50 +0100

    Add counters to user and post models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_215117) do
+ActiveRecord::Schema.define(version: 2022_01_26_220924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_215117) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.integer "comments_counter"
+    t.integer "likes_counter"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -50,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_215117) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
