d49d28ea98b1b2dbdcbf5d143623e8d0c8132922
wuyepabdul <wuyepabdul@gmail.com>
2022-01-27 00:14:53 +0100

    Add update_likes_counter in likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
     belongs_to :post, counter_cache: :likes_counter
     belongs_to :user
+
+    def update_likes_counter
+        post.likes_counter = 0 if post.likes_counter.nil?
+        posts.likes_counter += 1
+        posts.save
+    end
 end
