b0b1d4e06d8d29e408e954c396b17f855a3475f0
wuyepabdul <wuyepabdul@gmail.com>
2022-01-29 00:02:30 +0100

    Add c create comments and create post functionalities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,28 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.new(post_params)
+    respond_to do |format|
+      format.html do
+        if post.save
+          redirect_to user_post_path(post.author.id, post.id), notice: 'Published successfully!'
+        else
+          flash.now[:alert] = 'Failed to publish post!'
+          render :new
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
