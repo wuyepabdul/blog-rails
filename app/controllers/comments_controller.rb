37e81864248cb23e0a25c3e2dfbd22d3e9b0d23d
wuyepabdul <wuyepabdul@gmail.com>
2022-01-29 13:46:06 +0100

    Add little css to views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html do
         if comment.save
-          
+
           redirect_to user_post_path(post.author.id, post.id), notice: 'Comment added!'
         else
           flash.now[:error] = 'Faild to send comment!'
