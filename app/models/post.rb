86f3f32215c3213a07cb43137f744bfb8ad9f46e
Victor <66097371+VICTOR-CHUKWU@users.noreply.github.com>
2022-02-03 20:12:36 +0100

    Update post.rb
    Added validation to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250, too_long: 'Title length must not exceed 250 characters' }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  
   belongs_to :author, class_name: 'User', counter_cache: :posts_counter
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
