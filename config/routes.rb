b43996d6199f0727109f6be1361e2803585c305d
victor-chukwu <okenwa1993@gmail.com>
2022-02-05 17:58:27 +0100

    Added Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+             }
+  get '/member-data', to: 'members#show'
+end
   get 'likes/create'
   get 'comments/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
