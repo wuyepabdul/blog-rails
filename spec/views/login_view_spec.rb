11ff1d55b78ad65674197db0c4ec95fd8adab522
wuyepabdul <wuyepabdul@gmail.com>
2022-02-05 17:19:20 +0100

    Revert project to working state

--- spec/views/login_view_spec.rb
+++ spec/views/login_view_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
+
 describe 'Login', type: :feature do
   before(:each) do
     visit user_session_path
   end
+
   describe 'login page' do
     it 'has username and password inputs' do
       expect(page).to have_field 'Email'
       expect(page).to have_field 'Password'
       expect(page).to have_button 'Log in'
     end
+
     it 'displays an error when fields are empty' do
       click_button 'Log in'
       expect(page).to have_content 'Invalid Email or password'
     end
+
     it 'displays an error with the wrong username or password' do
       user = User.new(name: 'Test', email: 'test@gmail.com', password: 'test123')
       user.skip_confirmation!
@@ -22,6 +26,7 @@ describe 'Login', type: :feature do
       click_button 'Log in'
       expect(page).to have_content 'Invalid Email or password'
     end
+
     it 'correctly signs new user with correct password in' do
       user = User.new(name: 'Test', email: 'test@gmail.com', password: 'test123')
       user.skip_confirmation!
@@ -31,6 +36,7 @@ describe 'Login', type: :feature do
       click_button 'Log in'
       expect(page).to have_content 'Signed in successfully.'
     end
+
     it 'correctly signs new user with correct password in' do
       user = User.new(name: 'Test', email: 'test@gmail.com', password: 'test123')
       user.skip_confirmation!
--- spec/views/posts_show_spec.rb
+++ spec/views/posts_show_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 describe 'In Post Show Page', type: :feature do
   before(:each) do
     visit user_session_path
@@ -9,6 +10,7 @@ describe 'In Post Show Page', type: :feature do
     fill_in 'Email', with: 'user1@gmail.com'
     fill_in 'Password', with: 'user1user1'
     click_button 'Log in'
+
     post = @user1.posts.create!(
       title: 'Cats and Dogs', text: 'this is first post about cats and dogs!',
       comments_counter: 0, likes_counter: 0
@@ -23,25 +25,32 @@ describe 'In Post Show Page', type: :feature do
     user2.comments.create!(text: 'fantastic', post: post)
     visit user_post_path(@user1, post)
   end
+
   describe 'I can see' do
     it 'a post\'s title.' do
       expect(page).to have_content 'Cats and Dogs'
     end
+
     it 'who wrote the post.' do
       expect(page).to have_content 'user1'
     end
+
     it 'how many comments it has.' do
       expect(page).to have_content 'Comments: 3'
     end
+
     it 'how many likes it has.' do
       expect(page).to have_content 'Likes: 0'
     end
+
     it 'the post body.' do
       expect(page).to have_content 'this is first post about cats and dogs!'
     end
+
     it 'the username of each commentor.' do
       expect(page).to have_content 'user2'
     end
+
     it 'the comment each commentor left.' do
       expect(page).to have_content 'really nice post Michael'
       expect(page).to have_content 'Great'
